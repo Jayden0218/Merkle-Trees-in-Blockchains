a67a18efadf686a9b1fad30c83e4cd7e9149d4ce
Jayden0218 <jaydenooi0223@gmail.com>
2024-05-21 14:20:04 +0800

    first commit

---
+++ client/index.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+
+const serverUrl = 'http://localhost:1225';
+
+let userInput = "Sidney Kertzmann"
+
+async function main() {
+  // TODO: how do we prove to the server we're on the nice list? 
+  
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    // TODO: add request body parameters here!
+    userInput
+  });
+
+  console.log({ gift });
+}
+
+main();
